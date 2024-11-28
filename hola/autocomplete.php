04e479e8795ce60d09a42a25be0a56c81c08911b
MrDaplyere <129137538+MrDaplyere@users.noreply.github.com>
2024-11-28 17:37:15 -0600

    Update autocomplete.php

--- hola/autocomplete.php
+++ hola/autocomplete.php
@@ -7,16 +7,29 @@ $database = new Database();
 $db = $database->getConnection();
 $salesReportFacade = new SalesReportFacade($db);
 
-if (isset($_GET['term'])) {
+if (isset($_GET['term']) && isset($_GET['type'])) {
     $term = $_GET['term'];
-    $products = $salesReportFacade->getProductsReport();
+    $type = $_GET['type'];
 
-    // Filtrar productos según el término buscado
-    $filteredProducts = array_filter($products, function($product) use ($term) {
-        return stripos($product['NombreProducto'], $term) !== false;
-    });
+    if ($type === 'product') {
+        $products = $salesReportFacade->getProductsReport();
 
-    echo json_encode(array_values($filteredProducts));
+        // Filtrar productos según el término buscado
+        $filteredProducts = array_filter($products, function($product) use ($term) {
+            return stripos($product['NombreProducto'], $term) !== false;
+        });
+
+        echo json_encode(array_values($filteredProducts));
+    } elseif ($type === 'client') {
+        $clients = $salesReportFacade->getClientsReport();
+
+        // Filtrar clientes según el término buscado
+        $filteredClients = array_filter($clients, function($client) use ($term) {
+            return stripos($client['Nombre'], $term) !== false;
+        });
+
+        echo json_encode(array_values($filteredClients));
+    }
 } elseif (isset($_GET['action']) && $_GET['action'] === 'get_price' && isset($_GET['id'])) {
     $productId = $_GET['id'];
     $products = $salesReportFacade->getProductsReport();
