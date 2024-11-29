ab1d36f3aef171dcd2277e052bcc65fe8c433ba1
MrDaplyere <129137538+MrDaplyere@users.noreply.github.com>
2024-11-29 11:57:28 -0600

    Update index.php

--- hola/index.php
+++ hola/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../business/Presentador.php'; // Ruta absoluta desde el archivo actual
+require_once 'business/Presentador.php'; // Ruta correcta
 
 $presentador = new Presentador();
 
@@ -30,6 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 $reportData = $presentador->getSalesReport();
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
